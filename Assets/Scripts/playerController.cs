c0d13a42eb901dc3b8bad5e54487d6ee73b9fc45
Andrew Cualing <ayc300@nyu.edu>
2016-02-25 15:11:26 -0500

    added player controller, blocked out world

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+    CharacterController myController;
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        myController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.forward * speed * vertical * Time.deltaTime;
+        myController.Move(movement + Physics.gravity * Time.deltaTime);
+
+        transform.Rotate(0f, horizontal * 90f * Time.deltaTime, 0f);
+	}
+}
